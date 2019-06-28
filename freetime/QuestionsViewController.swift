a95461401237430c6d18b814fe9e5e694ae3f3bc
arisamuto <cookiemonster.ca17@gmail.com>
2019-06-28 22:08:01 +0900

    working

--- freetime/QuestionsViewController.swift
+++ freetime/QuestionsViewController.swift
@@ -10,31 +10,71 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
     
-    var quizArray = [[Any]] ()
-    var correctAnswer: Int = 0
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var choiceButtons: [UIButton]!
+   
     
-    @IBOutlet var textview: UITextView!
-    @IBOutlet var choiceButtons1: UIButton!
-    @IBOutlet var choiceButtons2: UIButton!
-    @IBOutlet var choiceButtons3: UIButton!
-    @IBOutlet var choiceButtons4: UIButton!
+    var quiznumber = 0
+    var correctanswers = 0
+    
+    var csvArray: [AnyObject] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func csvPath() {
+        if let csvPath = Bundle.main.path(forResource: "問題", ofType: "csv"){
+            do{
+                let csvStr = try
+                    String(contentsOfFile: csvPath, encoding: String.Encoding.utf8)
+                let csvArr = csvStr.split(separator: "\n").map(String.init)
+                
+                for csvFile in csvArr {
+                    let csvSplit = csvFile.components(separatedBy: ",")
+                    csvArray.append(csvSplit as AnyObject)
+                }
+                print(csvArray)
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
     }
-    */
+    
+    func questions () {
+        if quiznumber >= csvArray.count {
+            let alert = UIAlertController(title: "", message: "", preferredStyle:.alert)
+            let action = UIAlertAction(title: "", style: .default, handler: { (_) in self.dismiss(animated: true, completion: nil)})
+            alert.addAction(action)
+            present(alert, animated: true,completion: nil)
+            return
+        }
+        
+        let quizdata = csvArray[quiznumber]
+        label1.text = "第" + "\(quiznumber+1)問"
+        label2.text = (quizdata[0] as AnyObject as! String)
+        
+        let number1 = numberlist()
+        for i in 0...30 {
+            choiceButtons[i].setTitle(quizdata[number1[i]] as AnyObject as? String, for: .normal)
+        }
+        
+    }
+    
+    func numberlist () -> [Int] {
+        var list = [1,2,3,4]
+        for _ in 1...10 {
+            let i1 = Int(arc4random() % 4)
+            let i2 = Int(arc4random() % 4)
+            if i1 != i2 {
+                list.swapAt(i1,i2)
+            }
+        }
+        return list
+    }
+   
     @IBAction func ButtonCheck(_ sender: Any) {
     }
 
